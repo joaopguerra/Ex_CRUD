18725793f700ab5dd153eadb878ae385a462c86e
Joao Guerra <jompamg@gmail.com>
2019-03-25 11:16:06 -0300

    Primeira versao da pratica de CRUD

---
+++ Ex_Crud/Cadastro.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Ex_Crud
+{
+    partial class formCadastro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.titulo = new System.Windows.Forms.Label();
+            this.Código = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.Estado = new System.Windows.Forms.Label();
+            this.Cidade = new System.Windows.Forms.Label();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.cmbCidade = new System.Windows.Forms.ComboBox();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(82, 196);
+            this.btnInserir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(100, 28);
+            this.btnInserir.TabIndex = 0;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.Location = new System.Drawing.Point(164, 11);
+            this.titulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(204, 20);
+            this.titulo.TabIndex = 1;
+            this.titulo.Text = "CADASTRO DE USUÁRIO";
+            // 
+            // Código
+            // 
+            this.Código.AutoSize = true;
+            this.Código.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Código.Location = new System.Drawing.Point(14, 50);
+            this.Código.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Código.Name = "Código";
+            this.Código.Size = new System.Drawing.Size(41, 17);
+            this.Código.TabIndex = 2;
+            this.Código.Text = "Cód.:";
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nome.Location = new System.Drawing.Point(14, 78);
+            this.Nome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(49, 17);
+            this.Nome.TabIndex = 3;
+            this.Nome.Text = "Nome:";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email.Location = new System.Drawing.Point(13, 111);
+            this.Email.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(46, 17);
+            this.Email.TabIndex = 4;
+            this.Email.Text = "Email:";
+            // 
+            // Estado
+            // 
+            this.Estado.AutoSize = true;
+            this.Estado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Estado.Location = new System.Drawing.Point(13, 146);
+            this.Estado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Estado.Name = "Estado";
+            this.Estado.Size = new System.Drawing.Size(56, 17);
+            this.Estado.TabIndex = 5;
+            this.Estado.Text = "Estado:";
+            // 
+            // Cidade
+            // 
+            this.Cidade.AutoSize = true;
+            this.Cidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cidade.Location = new System.Drawing.Point(241, 146);
+            this.Cidade.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Cidade.Name = "Cidade";
+            this.Cidade.Size = new System.Drawing.Size(56, 17);
+            this.Cidade.TabIndex = 6;
+            this.Cidade.Text = "Cidade:";
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Location = new System.Drawing.Point(76, 143);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(68, 24);
+            this.cmbEstado.TabIndex = 7;
+            // 
+            // cmbCidade
+            // 
+            this.cmbCidade.FormattingEnabled = true;
+            this.cmbCidade.Location = new System.Drawing.Point(299, 143);
+            this.cmbCidade.Name = "cmbCidade";
+            this.cmbCidade.Size = new System.Drawing.Size(199, 24);
+            this.cmbCidade.TabIndex = 8;
+            // 
+            // txtCod
+            // 
+            this.txtCod.Location = new System.Drawing.Point(77, 50);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(100, 23);
+            this.txtCod.TabIndex = 9;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(77, 80);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(421, 23);
+            this.txtNome.TabIndex = 10;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(76, 111);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(422, 23);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Location = new System.Drawing.Point(205, 196);
+            this.btnDeletar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(100, 28);
+            this.btnDeletar.TabIndex = 12;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnBuscar.Location = new System.Drawing.Point(197, 50);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(68, 22);
+            this.btnBuscar.TabIndex = 13;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(330, 195);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(100, 28);
+            this.btnEditar.TabIndex = 14;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // formCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 263);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.cmbCidade);
+            this.Controls.Add(this.cmbEstado);
+            this.Controls.Add(this.Cidade);
+            this.Controls.Add(this.Estado);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.Código);
+            this.Controls.Add(this.titulo);
+            this.Controls.Add(this.btnInserir);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "formCadastro";
+            this.Text = "CADASTRO";
+            this.Load += new System.EventHandler(this.formCadastro_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.Label Código;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label Estado;
+        private System.Windows.Forms.Label Cidade;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.ComboBox cmbCidade;
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEditar;
+    }
+}
+
---
+++ Ex_Crud/Cadastro.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Ex_Crud
+{
+    public partial class formCadastro : Form
+    {
+        MySqlConnection conexao = new MySqlConnection("SERVER=localhost;DATABASE=bd_crud;UID=root;PASSWORD=;");
+
+
+        public formCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao.Open();
+                MySqlCommand comandos = new MySqlCommand("insert into tb_dados(codigo, nome, email, cidade, estado) values (null, ?, ?, ?, ?)", conexao);
+                comandos.Parameters.Add("@nome", MySqlDbType.VarChar, 30).Value = txtNome.Text;
+                comandos.Parameters.Add("@email", MySqlDbType.VarChar, 30).Value = txtEmail.Text;
+                comandos.Parameters.Add("@cidade", MySqlDbType.VarChar, 30).Value = cmbCidade.SelectedItem.ToString();
+                comandos.Parameters.Add("@estado", MySqlDbType.VarChar, 2).Value = cmbEstado.SelectedItem.ToString();
+
+                txtNome.Text = "";
+                txtEmail.Text = "";
+                cmbCidade.Text = "";
+                cmbEstado.Text = "";
+
+                comandos.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastrado com sucesso!");
+                conexao.Close();
+            }
+
+            catch
+            {
+                MessageBox.Show("Não conectado");
+            }
+        }
+
+        private void formCadastro_Load(object sender, EventArgs e)
+        {
+            cmbEstado.Items.Add("CE"); cmbEstado.Items.Add("BA"); cmbEstado.Items.Add("PE");
+            cmbCidade.Items.Add("Fortaleza"); cmbCidade.Items.Add("Salvador"); cmbCidade.Items.Add("Recife");
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //abre o banco
+                conexao.Open();
+                //comandos do mysql com seus devidos parametros
+                MySqlCommand comandos = new MySqlCommand("delete from tb_dados where codigo = ?", conexao);
+                comandos.Parameters.Clear();
+                comandos.Parameters.Add("@codigo", MySqlDbType.Int32).Value = txtCod.Text;
+
+                txtCod.Text = "";
+                txtNome.Text = "";
+                txtEmail.Text = "";
+                cmbCidade.Text = "";
+                cmbEstado.Text = "";
+
+                //executa o comando
+                comandos.CommandType = CommandType.Text;
+                comandos.ExecuteNonQuery();
+                //fecha a conexão
+                conexao.Close();
+                MessageBox.Show("Registro deletado com sucesso!");
+
+            }
+
+            catch (Exception erro)
+            {
+                MessageBox.Show("Não foi possível deletar" + erro);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //abre o banco
+                conexao.Open();
+                //comandos do mysql com seus devidos parametros
+                MySqlCommand comandos = new MySqlCommand("select nome, email, estado, cidade from tb_dados where codigo = ?", conexao);
+                comandos.Parameters.Clear();
+                comandos.Parameters.Add("@codigo", MySqlDbType.Int32).Value = txtCod.Text;
+
+                //executa o comando
+                comandos.CommandType = CommandType.Text;
+                //recebe o conteúdo que vem do banco
+                MySqlDataReader dr;
+                dr = comandos.ExecuteReader();
+                //insere as informações recebidas do banco para os componentes do form
+                dr.Read();
+                txtNome.Text = dr.GetString(0);
+                txtEmail.Text = dr.GetString(1);
+                cmbEstado.Text = dr.GetString(2);
+                cmbCidade.Text = dr.GetString(3);
+
+
+                //fecha a conexão
+                conexao.Close();
+            }
+
+            catch (Exception erro)
+            {
+                MessageBox.Show("Código não encontrado" + erro);
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //abre o banco
+                conexao.Open();
+                //comandos do mysql com seus devidos parametros
+                MySqlCommand comandos = new MySqlCommand("update tb_dados set nome = ?, email = ?, cidade = ?, estado = ? where codigo = ?", conexao);
+                comandos.Parameters.Clear();
+                comandos.Parameters.Add("@nome", MySqlDbType.VarChar, 30).Value = txtNome.Text;
+                comandos.Parameters.Add("@email", MySqlDbType.VarChar, 30).Value = txtEmail.Text;
+                comandos.Parameters.Add("@cidade", MySqlDbType.VarChar, 30).Value = cmbCidade.SelectedItem.ToString();
+                comandos.Parameters.Add("@estado", MySqlDbType.VarChar, 2).Value = cmbEstado.SelectedItem.ToString();
+                comandos.Parameters.Add("@codigo", MySqlDbType.Int32).Value = txtCod.Text;
+
+                //limpa os campos
+                txtCod.Text = "";
+                txtNome.Text = "";
+                txtEmail.Text = "";
+                cmbCidade.Text = "";
+                cmbEstado.Text = "";
+
+                //executando comando
+                comandos.CommandType = CommandType.Text;
+                comandos.ExecuteNonQuery();
+
+                //fecha a conexão
+                conexao.Close();
+
+                MessageBox.Show("Editado com sucesso!");
+            }
+
+            catch (Exception erro)
+            {
+                MessageBox.Show("Falha ao editar" + erro);
+            }
+        }
+    }
+}
